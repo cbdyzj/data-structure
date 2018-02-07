7f62abc1759e28caff335b9b906fe925db7dcca1
cbdyzj <cbdyzj@jianzhao.org>
2018-02-07 14:45:44 +0800

    add Array and Queue

---
+++ array/DSArray.ts
@@ -0,0 +1,26 @@
+class DSArray<T> {
+
+    container: T[] = []
+
+    add(data: T) {
+        this.container.push(data)
+    }
+
+    remove(data: T) {
+        this.container = this.container.filter(c => c !== data)
+    }
+
+    set(index: number, data: T) {
+        this.container[index] = data
+    }
+
+    get(index: number) {
+        return this.container[index]
+    }
+
+    size() {
+        return this.container.length
+    }
+}
+
+export default DSArray
---
+++ queue/DSQueue.ts
@@ -0,0 +1,22 @@
+class DSQueue<T> {
+
+    container: T[] = []
+
+    push(data: T) {
+        this.container.push(data)
+    }
+
+    pop() {
+        return this.container.pop()
+    }
+
+    peak() {
+        return this.container[this.container.length - 1]
+    }
+
+    size() {
+        return this.container.length
+    }
+}
+
+export default DSQueue
