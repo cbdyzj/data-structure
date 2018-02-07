47aabd176757d2bdd8d35bf5e03c2cf165a99ca1
cbdyzj <cbdyzj@jianzhao.org>
2018-02-07 17:43:35 +0800

    update Traversals

--- tree/BinaryTree.ts
+++ tree/BinaryTree.ts
@@ -33,6 +33,25 @@ export const Traversals = {
         Traversals.postOrder(node.left, fn)
         Traversals.postOrder(node.right, fn)
         fn(node.data)
+    },
+
+    levelOrder<T>(root: TreeNode<T>, fn: Operate<T>) {
+        const queue: TreeNode<T>[] = []
+        let node = root
+        if (!node) {
+            return
+        }
+        queue.push(node)
+        while (queue.length) {
+            node = queue.shift()
+            fn(node.data)
+            if (node.left) {
+                queue.push(node.left)
+            }
+            if (node.right) {
+                queue.push(node.right)
+            }
+        }
     }
 
 }
