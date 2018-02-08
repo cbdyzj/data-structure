cce7f8ca644c5d9cf8486ef2c898f780e811884e
cbdyzj <cbdyzj@jianzhao.org>
2018-02-08 14:12:10 +0800

    add dfs

--- graph/UndirectedGraph.ts
+++ graph/UndirectedGraph.ts
@@ -10,7 +10,7 @@ export interface AdjacencyListNode<T> {
 export type Operate<T> = (data: T) => void
 
 // 广度优先遍历
-function bfs<T>(start: AdjacencyListNode<T>, fn: Operate<T>) {
+export function bfs<T>(start: AdjacencyListNode<T>, fn: Operate<T>) {
     const queue: AdjacencyListNode<T>[] = []
     const visited = new Map<AdjacencyListNode<T>, boolean>()
     queue.push(start)
@@ -19,7 +19,22 @@ function bfs<T>(start: AdjacencyListNode<T>, fn: Operate<T>) {
         if (!visited.get(current)) {
             fn(current.data)
             visited.set(current, true)
-            queue.push(...current.adjacent)
+            queue.push(...current.adjacent.filter(c => !visited.get(c)))
+        }
+    }
+}
+
+// 深度优先遍历
+export function dfs<T>(start: AdjacencyListNode<T>, fn: Operate<T>) {
+    const stack: AdjacencyListNode<T>[] = []
+    const visited = new Map<AdjacencyListNode<T>, boolean>()
+    stack.push(start)
+    while (stack.length) {
+        const current = stack.pop()
+        if (!visited.get(current)) {
+            fn(current.data)
+            visited.set(current, true)
+            stack.push(...current.adjacent.filter(c => !visited.get(c)))
         }
     }
 }
