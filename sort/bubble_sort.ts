5281d0f6efd02099159368fb6357067727077da6
Cbdy <cbdyzj@jianzhao.org>
2019-06-17 20:27:45 +0800

    -

--- sort/bubble_sort.ts
+++ sort/bubble_sort.ts
@@ -4,9 +4,6 @@
  * @param arr Array to sort
  */
 function bubbleSort(arr) {
-    if (arr.length <= 1) {
-        return arr
-    }
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0, k = 1; k < arr.length - i; j++ , k++) {
             if (arr[j] > arr[k]) {
