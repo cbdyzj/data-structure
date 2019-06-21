8afc64dc3e06213cff5b3c6252b6e9053f96913f
cbdyzj <cbdyzj@jianzhao.org>
2019-06-21 19:37:04 +0800

    Add leetcode 5

---
+++ leetcode/5.js
@@ -0,0 +1,51 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function isPalindrome(s, i, j) {
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            return false
+        }
+        i++
+        j--
+    }
+    return true
+}
+
+function min(a, b) {
+    return a > b ? b : a
+}
+
+var longestPalindrome = function (s) {
+    if (isPalindrome(s, 0, s.length - 1)) {
+        return s
+    }
+    var lp = ''
+    for (var i = 0; i < s.length * 2 - 1; i++) {
+        if (i % 2 === 0) {
+            // odd
+            var tm = min(i / 2, s.length - i / 2 - 1)
+            for (var k = tm; k >= 0; k--) {
+                if (isPalindrome(s, i / 2 - k, i / 2 + k)) {
+                    if (k * 2 + 1 > lp.length) {
+                        lp = s.substring(i / 2 - k, i / 2 + k + 1)
+                        break
+                    }
+                }
+            }
+        } else {
+            // even
+            var tm = min((i + 1) / 2, s.length - (i + 1) / 2)
+            for (var k = tm; k >= 0; k--) {
+                if (isPalindrome(s, (i - 1) / 2 - k, (i + 1) / 2 + k)) {
+                    if ((k + 1) * 2 > lp.length) {
+                        lp = s.substring((i - 1) / 2 - k, (i + 1) / 2 + k + 1)
+                        break
+                    }
+                }
+            }
+        }
+    }
+    return lp
+};
