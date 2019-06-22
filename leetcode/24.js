2f4786b103ba5a80190138327972866497c26a46
cbdyzj <cbdyzj@jianzhao.org>
2019-06-22 20:06:26 +0800

    Amend

--- leetcode/24.js
+++ leetcode/24.js
@@ -26,3 +26,29 @@ var swapPairs = function (head) {
 };
 
 // 递归
+
+var swapPairs = function (head) {
+    if (!head) {
+        return null
+    }
+    let p = head
+    let q = p.next
+    if (!q) {
+        return p
+    }
+    head = p.next
+    p.next = q.next
+    head.next = p
+
+    q = p.next
+    while (p.next && q.next) {
+        p.next = q.next
+        q.next = p.next.next
+        p.next.next = q
+        p = q
+        q = p.next
+    }
+    return head
+};
+
+// 循环
---
+++ leetcode/26.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let count = 0
+    for (let i = 0; i < nums.length - count; i++) {
+        let p = 0
+        while (i + p + 1 < nums.length - count
+            && nums[i] === nums[i + p + 1]) {
+            p++
+        }
+        if (p) {
+            for (let j = 1; i + j + p < nums.length - count; j++) {
+                nums[i + j] = nums[i + j + p]
+            }
+            count += p
+        }
+    }
+    return nums.length - count
+};
