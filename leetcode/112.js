a10c679702a3122d293403f5f73115c4cda85c3a
cbdyzj <cbdyzj@jianzhao.org>
2019-06-27 11:43:14 +0800

    112, 113

---
+++ leetcode/112.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+function childSum(node, acc, sum) {
+    if (!node) {
+        return false
+    }
+    const acc2 = node.val + acc
+    if (acc2 === sum) {
+        if (!node.left && !node.right) {
+            return true
+        }
+    }
+    if (!node.left && !node.right) {
+        return false
+    }
+    return childSum(node.left, acc2, sum)
+        || childSum(node.right, acc2, sum)
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, sum) {
+    return childSum(root, 0, sum)
+};
---
+++ leetcode/113.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+function childSum(node, acc, sum) {
+    if (!node) {
+        return [[false]]
+    }
+    const acc2 = node.val + acc
+    if (acc2 === sum) {
+        if (!node.left && !node.right) {
+            return [[true, node.val]]
+        }
+    }
+    if (!node.left && !node.right) {
+        return [[false]]
+    }
+    const leftRes = childSum(node.left, acc2, sum)
+        .filter(item => item[0])
+        .map(item => {
+            item.push(node.val)
+            return item
+        })
+    const rightRes = childSum(node.right, acc2, sum)
+        .filter(item => item[0])
+        .map(item => {
+            item.push(node.val)
+            return item
+        })
+    return [...leftRes, ...rightRes]
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function (root, sum) {
+    return childSum(root, 0, sum)
+        .filter(item => item[0])
+        .map(item => {
+            item.shift()
+            return item.reverse()
+        })
+};
+
+// 根据112简单改一下
+// 本质是DFS
